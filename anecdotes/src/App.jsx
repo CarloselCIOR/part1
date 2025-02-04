624175dac3f08195475140d725a7cb7870b8ee83
Carlos Ismael Orozco Rios <carloselcior@gmail.com>
2025-02-04 10:50:32 -0700

    1.13

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -13,10 +13,19 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+
+      <button onClick={() => {
+        const newVotes = [...votes];
+        newVotes[selected] += 1;
+        setVotes(newVotes);
+      }}>vote</button>
+
       <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>next anecdote</button>
     </div>
   )
